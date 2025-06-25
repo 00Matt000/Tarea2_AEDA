153a1307b06f6d0ccf4f02476cbef01b2f655dd6
Rodrigo Ignacio Jara Rodríguez <105901543+00Matt000@users.noreply.github.com>
2025-06-25 15:16:19 -0400

    Update Numeros_Primos_Hilos.cpp

--- Numeros_Primos_Hilos.cpp
+++ Numeros_Primos_Hilos.cpp
@@ -6,10 +6,10 @@
 #include <chrono>
 
 bool isPrime(long n) {
-    if (n <= 1) return false;
+    if (n < 2) return false;
     if (n == 2) return true;
     if (n % 2 == 0) return false;
-    for (long i = 3; i <= std::sqrt(n); i += 2) {
+    for (long i = 3; i * i <= n; i += 2) {
         if (n % i == 0) return false;
     }
     return true;
